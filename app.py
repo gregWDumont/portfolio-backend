b8f730412ac68affd6be016359ef8c22dd64bcc8
gregWDumont <gregory.wd@pm.me>
2023-08-09 18:04:28 +0200

    Update for Heroku deployment

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -15,4 +16,6 @@ def get_projects():
     return jsonify(data["projects"])
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
