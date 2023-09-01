2467917a91f5333e0efa272fa2896af0a82a5955
Gregory Wawszyniak Dumont <gregory.wd@pm.me>
2023-09-01 02:44:48 +0200

    Refactor function with correct link + mkdir

--- fetch_svg_github_activity.py
+++ fetch_svg_github_activity.py
@@ -10,15 +10,15 @@ def fetch_svg_github_activity():
         return
 
     headers = {'Authorization': f'token {token}'}
-    svg_url = 'https://github.com/gregWDumont/gregWDumont/blob/main/github-contribution-grid-snake-dark.svg'
+    svg_url = 'https://raw.githubusercontent.com/gregWDumont/gregWDumont/main/github-contribution-grid-snake-dark.svg'
     
     r = requests.get(svg_url, headers=headers)
     
     if r.status_code == 200:
-        content = r.json().get('content')
-        if content:
-            decoded_content = base64.b64decode(content)
-            with open('static/your-svg-file.svg', 'wb') as f:
-                f.write(decoded_content)
+        # Create static folder if it doesn't exist
+        Path("static").mkdir(exist_ok=True)
+        
+        with open('static/github-contribution-grid-snake-dark.svg', 'wb') as f:
+            f.write(r.content)  # Use r.content to get binary content
     else:
         print(f"Failed to fetch SVG: {r.status_code}, {r.text}")
